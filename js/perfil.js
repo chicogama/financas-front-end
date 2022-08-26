5c2f0b176d1325efb7962d2ab422cba63a9dc77d
chicogama <josemargama7@gmail.com>
2022-08-26 16:44:02 -0300

    show perfil info

--- js/perfil.js
+++ js/perfil.js
@@ -10,13 +10,30 @@ function getPerfil(url) {
     dadosUsuario = xhttp.responseText;
 
     console.log(dadosUsuario)
+    perfil = JSON.parse(dadosUsuario)
+
 
     var meuNome = document.getElementById("meunome").value;
-    var meu = JSON.stringify(dadosUsuario);
-    document.getElementById('meunome').value = meu;
+    var meuEmail = document.getElementById("meuemail").value;
+    var meuCPF = document.getElementById("meucpf").value;
+    var meuNasc = document.getElementById("meunascimento").value;
+
+    var data = new Date(perfil.dataNascimento);
+    console.log(data)
+    var month = data.getUTCMonth() + 1; //months from 1-12
+    var day = data.getUTCDate();
+    var year = data.getUTCFullYear();
+    newdate = year + "-0" + month + "-" + day;
+
+    console.log(newdate)
+
+    document.getElementById('meunome').value = perfil.nome;
+    document.getElementById('meuemail').value = perfil.email;
+    document.getElementById('meucpf').value = perfil.cpf;
+    document.getElementById('meunascimento').value = newdate;
 
 
 }
 
 
-getPerfil("https://api-financa.herokuapp.com/api/Usuario")
+getPerfil("https://api-financa.herokuapp.com/api/Usuario");
